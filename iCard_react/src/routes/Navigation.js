3a39ecb722c5e21ec0f790aac8f2b99b0a98bfab
DevepMau <oshiettecs@gmail.com>
2024-08-19 13:33:31 -0300

    Configuracion inicial de estructura de routes - Instalacion de react-router-dom

---
+++ iCard_react/src/routes/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+
+export function Navigation() {
+  return (
+    <Router>
+        <Routes>Navigation</Routes>
+    </Router>
+  )
+}
