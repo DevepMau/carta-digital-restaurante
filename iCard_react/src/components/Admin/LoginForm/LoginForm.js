7a490c9a9601e24d3c076792e75ca44d98c7895c
DevepMau <oshiettecs@gmail.com>
2024-08-21 21:59:51 -0300

    Estado y validacion del Login usando Yup y Formik

--- iCard_react/src/components/Admin/LoginForm/LoginForm.js
+++ iCard_react/src/components/Admin/LoginForm/LoginForm.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import { Button, Form } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import './LoginForm.scss';
 
-export function LoginForm() {
+export function LoginForm({ initialEmail = '', initialPassword = '' }) {
+  const formik = useFormik({
+    initialValues: {
+      email: initialEmail,
+      password: initialPassword,
+    },
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formValue) => {
+      console.log("login OK");
+      console.log(formValue);
+    }
+  });
+
   return (
-    <Form className='login-form-admin'>
-        <Form.Input name='email' placeholder='Correo electronico' />
-        <Form.Input name='password' type='password' placeholder='Contraseña' />
+    <Form className='login-form-admin' onSubmit={formik.handleSubmit}>
+        <Form.Input 
+          name='email' 
+          placeholder='Correo electronico' 
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+        />
+        <Form.Input 
+          name='password' 
+          type='password' 
+          placeholder='Contraseña'
+          value={formik.values.password}
+          onChange={formik.handleChange} 
+          error={formik.errors.password}
+        />
         <Button type='submit' content='Iniciar sesion' primary fluid />
     </Form>
-  )
+  );
 }
+
+function validationSchema(){
+  return {
+    email: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  };
+}
+
