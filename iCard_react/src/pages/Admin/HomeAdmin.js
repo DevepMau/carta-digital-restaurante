ffc729bd392b2ae404dcf4b4f9ca615d5364b182
DevepMau <oshiettecs@gmail.com>
2024-09-01 20:05:04 -0300

    Se implemento el logout del context

--- iCard_react/src/pages/Admin/HomeAdmin.js
+++ iCard_react/src/pages/Admin/HomeAdmin.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useAuth } from '../../hooks';
 
 export function HomeAdmin() {
+  const { logout } = useAuth();
   return (
-    <div>HomeAdmin</div>
+    <div>
+      <h1>HOME ADMIN</h1>
+      <button onClick={logout}>Cerrar sesion</button>
+    </div>
   )
 }
