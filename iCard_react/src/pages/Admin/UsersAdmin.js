7afc3de09f354eee92f86a3dad723ad77c3dffd4
DevepMau <oshiettecs@gmail.com>
2024-09-02 11:39:35 -0300

    Obtener todos los usuarios

--- iCard_react/src/pages/Admin/UsersAdmin.js
+++ iCard_react/src/pages/Admin/UsersAdmin.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useUser } from '../../hooks';
 
 export function UsersAdmin() {
+    const { loading, users, getUsers } = useUser();
+    console.log('loading -->', loading);
+    console.log('users -->', users);
+
+    useEffect(() => {
+      getUsers();
+      // eslint-disable-next-line
+    }, [])
+    
+
   return (
     <div>UsersAdmin</div>
   )
