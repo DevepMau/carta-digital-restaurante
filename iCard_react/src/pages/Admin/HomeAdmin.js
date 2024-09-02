53cbbc4d2628434408ce9ae980d1e46068d17d8f
DevepMau <oshiettecs@gmail.com>
2024-09-02 10:38:34 -0300

    Se creo la estructura del SideMenu

--- iCard_react/src/pages/Admin/HomeAdmin.js
+++ iCard_react/src/pages/Admin/HomeAdmin.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import { useAuth } from '../../hooks';
 
 export function HomeAdmin() {
-  const { logout } = useAuth();
   return (
     <div>
       <h1>HOME ADMIN</h1>
-      <button onClick={logout}>Cerrar sesion</button>
     </div>
   )
 }
