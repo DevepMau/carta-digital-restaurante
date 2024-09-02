f7f31be58e7102662f56c57b564ea3c39781947d
DevepMau <oshiettecs@gmail.com>
2024-09-02 15:59:10 -0300

    Se creo un Modal basico

--- iCard_react/src/pages/Admin/UsersAdmin.js
+++ iCard_react/src/pages/Admin/UsersAdmin.js
@@ -1,22 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Loader } from 'semantic-ui-react';
 import { HeaderPage, TableUsers } from '../../components/Admin';
+import { ModalBasic } from '../../components/Common';
 import { useUser } from '../../hooks';
 
 export function UsersAdmin() {
+    const [showModal, setShowModal] = useState(false)
+    const [titleModal, setTitleModal] = useState(null)
+    const [contentModal, setContentModal] = useState(null)
+
     const { loading, users, getUsers } = useUser();
-    console.log('loading -->', loading);
-    console.log('users -->', users);
 
     useEffect(() => {
       getUsers();
       // eslint-disable-next-line
     }, [])
+
+    const openCloseModal = () => setShowModal((prev) => !prev)
     
 
   return (
     <div>
-        <HeaderPage title='Usuarios' btnTitle='Nuevo Usuario'/>
+        <HeaderPage 
+          title='Usuarios' 
+          btnTitle='Nuevo Usuario'
+          btnClick={openCloseModal}/>
         {loading ? (
             <Loader active inline='centered'>
                 Cargando...
@@ -24,6 +32,12 @@ export function UsersAdmin() {
         ) : (
             <TableUsers users={users}/>
         )}
+
+        <ModalBasic 
+          show={showModal} 
+          onClose={openCloseModal}
+          title='Crear usuario' 
+          children={<h2>Content...</h2>} />
     </div>
   )
 }
