99cac0d61a85975357529c1189c6863a59db95c1
DevepMau <oshiettecs@gmail.com>
2024-09-16 14:28:56 -0300

    Validacion del formulario de categorias

--- iCard_react/src/components/Admin/Category/AddEditCategoryFrom/AddEditCategoryFrom.js
+++ iCard_react/src/components/Admin/Category/AddEditCategoryFrom/AddEditCategoryFrom.js
@@ -1,13 +1,26 @@
 import React, { useCallback, useState } from 'react'
 import { Form, Image, Button } from 'semantic-ui-react';
 import { useDropzone } from 'react-dropzone';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import './AddEditCategoryFrom.scss';
 
 export function AddEditCategoryFrom() {
     const [previewImage, setPreviewImage] = useState(null);
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(newSchema()),
+        validateOnChange: false,
+        onSubmit: (formValue) => {
+            console.log("formulario enviado");
+            console.log(formValue);
+            
+        }
+    });
 
-    const onDrop = useCallback((acceptedFile) => {
+    const onDrop = useCallback(async (acceptedFile) => {
         const file = acceptedFile[0];
+        await formik.setFieldValue('image', file);
         setPreviewImage(URL.createObjectURL(file))
     }, []);
 
@@ -19,9 +32,18 @@ export function AddEditCategoryFrom() {
     });
   
     return (
-    <Form className='add-edit-category-form'>
-        <Form.Input name='title' placeholder='Nombre de la categoria' />
-        <Button type='button' fluid {...getRootProps()}>
+    <Form className='add-edit-category-form' onSubmit={formik.handleSubmit}>
+        <Form.Input 
+            name='title' 
+            placeholder='Nombre de la categoria' 
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            error={formik.errors.title}        />
+        <Button 
+            type='button' 
+            fluid 
+            color={formik.errors.image && "red"}
+            {...getRootProps()}>
             Subir imagen
         </Button>
         <input {...getInputProps()} />
@@ -29,4 +51,18 @@ export function AddEditCategoryFrom() {
         <Button type='submit' primary fluid content='Crear'/>
     </Form>
   )
+}
+
+function initialValues() {
+    return {
+        title: "",
+        image: "",
+    }
+}
+
+function newSchema() {
+    return {
+        title: Yup.string().required(true),
+        image: Yup.string().required(true),
+    };
 }
