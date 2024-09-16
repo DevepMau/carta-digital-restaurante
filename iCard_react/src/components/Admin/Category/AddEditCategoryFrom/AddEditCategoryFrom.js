c85c50d84f04baaf526fd3701d6dfa94ca965465
DevepMau <oshiettecs@gmail.com>
2024-09-16 10:18:54 -0300

    Se creo formulario para crear categoria y agregar imagen previa

---
+++ iCard_react/src/components/Admin/Category/AddEditCategoryFrom/AddEditCategoryFrom.js
@@ -0,0 +1,32 @@
+import React, { useCallback, useState } from 'react'
+import { Form, Image, Button } from 'semantic-ui-react';
+import { useDropzone } from 'react-dropzone';
+import './AddEditCategoryFrom.scss';
+
+export function AddEditCategoryFrom() {
+    const [previewImage, setPreviewImage] = useState(null);
+
+    const onDrop = useCallback((acceptedFile) => {
+        const file = acceptedFile[0];
+        setPreviewImage(URL.createObjectURL(file))
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: "image/jpeg, image/png",
+        noKeyboard: true,
+        multiple: false,
+        onDrop,
+    });
+  
+    return (
+    <Form className='add-edit-category-form'>
+        <Form.Input name='title' placeholder='Nombre de la categoria' />
+        <Button type='button' fluid {...getRootProps()}>
+            Subir imagen
+        </Button>
+        <input {...getInputProps()} />
+        <Image src={previewImage} fluid/>
+        <Button type='submit' primary fluid content='Crear'/>
+    </Form>
+  )
+}
--- iCard_react/src/pages/Admin/CategoriesAdmin.js
+++ iCard_react/src/pages/Admin/CategoriesAdmin.js
@@ -1,20 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Loader } from 'semantic-ui-react';
-import { HeaderPage, TableCategoryAdmin } from '../../components/Admin';
+import { HeaderPage, TableCategoryAdmin, AddEditCategoryFrom } from '../../components/Admin';
+import { ModalBasic } from '../../components/Common';
 import { useCategory } from '../../hooks';
 
 export function CategoriesAdmin() {
+    const [showModal, setShowModal] = useState(null);
+    const [titleModal, setTitleModal] = useState(null);
+    const [contentModal, setContentModal] = useState(null);
 
     const { categories, getCategories, loading } = useCategory();
     console.log(categories);
     // eslint-disable-next-line
     useEffect(() => getCategories(), [])
-    
 
+    const openCloseModal = () => setShowModal(prev => !prev);
+    
+    const addCategory = () => {
+      setTitleModal('Nueva categoria');
+      setContentModal(<AddEditCategoryFrom />);
+      openCloseModal()
+    }
 
   return (
     <>
-    <HeaderPage title='Categorias' btnTitle='Nueva categoria'/>
+    <HeaderPage title='Categorias' btnTitle='Nueva categoria' btnClick={addCategory}/>
     {loading ? (
         <Loader active inline='centered'>
             Cargando...
@@ -22,6 +32,13 @@ export function CategoriesAdmin() {
     ) : (
         <TableCategoryAdmin categories={categories} />
     )}
+
+    <ModalBasic
+      show = {showModal}
+      onClose = {openCloseModal}
+      title = {titleModal}
+      children = {contentModal}
+    />
     </>
   )
 }
